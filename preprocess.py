4bf812c73c3168027eb08285a51e4fd7348f6596
chenlb <1796390642@qq.com>
2024-10-22 12:31:38 +0800

    改为5折交叉验证

--- preprocess.py
+++ preprocess.py
@@ -4,7 +4,8 @@ import shutil
 import numpy as np
 import pandas as pd
 import h5py
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+
 from tqdm import tqdm
 from utils import *
 
@@ -22,7 +23,7 @@ class PreProcess:
         self.label_file = pd.read_csv("/mnt/chenlb/datasets/ADNI/raw_data/adni2_axial_3d_3_class_6_22_2024.csv")
         self.id_to_group = self.label_file.set_index("Image Data ID")["Group"].to_dict()
         self.group_to_num = {"CN": 0, "MCI": 1, "AD": 2}
-        self.target_shape = (45, 55, 45)# 目标形状
+        self.target_shape = (91, 109, 91)# 目标形状
         self.problematic_samples = []
         self.shape_statistics = []
         
@@ -84,58 +85,6 @@ class PreProcess:
                     register_image(fixed_image_path, input_path, output_file_path)
             except Exception as e:
                 self.problematic_samples.append((nii_file, str(e)))
-                
-                
-        # self.bbox_path = os.path.join(output_folder, "bbox")
-        # if not os.path.exists(self.bbox_path):
-        #     shutil.rmtree(self.bbox_path)
-
-        # os.makedirs(self.bbox_path, exist_ok=True)
-        
-        # for file_name in os.listdir(self.register_output_path):
-        #     file_path = os.path.join(self.register_output_path, file_name)
-        #     output_path = os.path.join(self.bbox_path, file_name)
-            
-        #     # 加载NIfTI文件
-        #     img = nib.load(file_path)
-        #     image_data = img.get_fdata()
-            
-        #     # 找到bounding box
-        #     bbox = find_bounding_box(image_data)
-        #     print(f'File: {file_name}, BBox: {bbox}')
-            
-        #     # 裁剪图像
-        #     cropped_image = crop_image(image_data, bbox)
-            
-        #     # 保存裁剪后的图像
-        #     cropped_img = nib.Nifti1Image(cropped_image, img.affine)
-        #     nib.save(cropped_img, output_path)
-
-        # bbox_list = []
-
-        # for file_name in os.listdir(self.register_output_path):
-        #     file_path = os.path.join(self.register_output_path, file_name)
-            
-        #     img = nib.load(file_path)
-        #     image_data = img.get_fdata()
-            
-        #     bbox = find_bounding_box(image_data)
-        #     bbox_list.append(bbox)
-
-        # max_bbox = get_max_bounding_box(bbox_list)
-        # self.logger.info(f'Maximum BBox: {max_bbox}')
-
-        # for file_name in os.listdir(self.register_output_path):
-        #     file_path = os.path.join(self.register_output_path, file_name)
-        #     output_path = os.path.join(self.bbox_path, file_name)
-            
-        #     img = nib.load(file_path)
-        #     image_data = img.get_fdata()
-            
-        #     cropped_image = crop_image(image_data, max_bbox)
-            
-        #     cropped_img = nib.Nifti1Image(cropped_image, img.affine)
-        #     nib.save(cropped_img, output_path)
 
         self.print_problematic_samples()
 
@@ -145,7 +94,7 @@ class PreProcess:
             shutil.rmtree(resized_save_path)
         os.makedirs(resized_save_path, exist_ok=True)
         
-        final_save_path = os.path.join(output_path, "generated_processed_45_55_45")
+        final_save_path = os.path.join(output_path, "generated_(91,109,91)_1miss")
         if os.path.exists(final_save_path):
             shutil.rmtree(final_save_path)
         os.makedirs(final_save_path, exist_ok=True)
@@ -164,42 +113,34 @@ class PreProcess:
                 continue
             try:
                 original_3d_image, original_shape, resized_shape, original_affine = process_nii_to_3d_array(subject_path, self.target_shape, return_original_shape=True)
-                self.shape_statistics.append({'subject': subject, 'original_shape': original_shape, 'resized_shape': resized_shape})
                 dataset_name = os.path.basename(subject_path)
                 data_entries.append({'dataset_name': dataset_name, 'data': original_3d_image, 'label': label})
                 
-                # Save resized NIfTI file with updated affine
-                resized_nii_file_path = os.path.join(resized_save_path, subject)
-                save_nifti_file(original_3d_image, original_affine, resized_nii_file_path)
             except Exception as e:
                 self.problematic_samples.append((subject_path, str(e)))
             pbar.update(1)
         
         pbar.close()
 
-        # Save shape statistics
-        shape_statistics_df = pd.DataFrame(self.shape_statistics)
-        shape_statistics_df.to_csv(os.path.join(final_save_path, "shape_statistics.csv"), index=False)
+        kf = KFold(n_splits=5, shuffle=True, random_state=42)
 
-        # 按8:2的比例分为训练集和测试集
-        train_entries, test_entries = train_test_split(data_entries, test_size=0.2, random_state=42)
+        fold_index = 1
+        for train_index, test_index in kf.split(data_entries):
+            train_entries = [data_entries[i] for i in train_index]
+            test_entries = [data_entries[i] for i in test_index]
 
-        # 将训练集中标签为1的样本移动到测试集中
-        train_entries_no_label1 = [entry for entry in train_entries if entry['label'] != 1]
-        label1_entries = [entry for entry in train_entries if entry['label'] == 1]
-        test_entries.extend(label1_entries)
+            # 删除train_entries中label为1的样本
+            train_entries = [entry for entry in train_entries if entry['label'] != 1]
 
-        # Save data
-        train_store_path = os.path.join(final_save_path, "train_data.h5")
-        train_label_path = os.path.join(final_save_path, "train_labels.csv")
-        test_store_path = os.path.join(final_save_path, "test_data.h5")
-        test_label_path = os.path.join(final_save_path, "test_labels.csv")
-        valid_store_path = os.path.join(final_save_path, "valid_data.h5")
-        valid_label_path = os.path.join(final_save_path, "valid_labels.csv")
-        self.save_data(train_entries_no_label1, train_store_path, train_label_path)
-        self.save_data(test_entries, valid_store_path, valid_label_path)
+            train_store_path = os.path.join(final_save_path, f"train_data_fold{fold_index}.h5")
+            train_label_path = os.path.join(final_save_path, f"train_labels_fold{fold_index}.csv")
+            test_store_path = os.path.join(final_save_path, f"test_data_fold{fold_index}.h5")
+            test_label_path = os.path.join(final_save_path, f"test_labels_fold{fold_index}.csv")
 
-        self.print_problematic_samples()
+            self.save_data(train_entries, train_store_path, train_label_path)
+            self.save_data(test_entries, test_store_path, test_label_path)
+
+            fold_index += 1
 
     def save_data(self, entries, data_path, label_path):
         with h5py.File(data_path, 'w') as data_store:
